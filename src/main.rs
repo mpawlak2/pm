c2a6e1bd0274b28e1ac668ff1151318fe2579640
Mateusz Pawlak <mpawlak62@gmail.com>
2025-07-16 20:35:33 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod pomo;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Start {
+
+    },
+}
+
+fn main() {
+    let cli = Cli::parse();
+}
